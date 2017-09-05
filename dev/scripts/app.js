43beb0684ea4185d1cf09f37a9c304b9c3f0e2e8
Matthew Parris <matthewparris@live.ca>
2017-09-05 11:37:35 -0400

    inital commit

---
+++ dev/scripts/app.js
@@ -0,0 +1,7 @@
+console.log('hello');
+
+// app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Fact: The best ninja turtle is Donatello</h1>, document.getElementById('app'));
